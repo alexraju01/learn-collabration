e6534adac5f6f05fe17f026876fdf246f2e25e59
Jamal Raja <Jamalraja083@gmail.com>
2025-10-08 09:49:15 +0100

    Create quiz.js and add link to quiz.html

---
+++ quiz.js
@@ -0,0 +1,6 @@
+// ============================================================
+// ======================= QUIZ SECTION =======================
+// ============================================================
+
+const quizContainer = document.getElementById("quiz-container")
+quizContainer.innerHTML = "lol"
